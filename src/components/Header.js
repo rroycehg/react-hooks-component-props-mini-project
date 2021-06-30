aa0379570adc9640124d0831d207339ebc2e0a79
Roli <rroycehg@gmail.com>
2021-06-30 18:11:15 -0400

    Finished

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+function Header (props) {
+    // const blogName = props.blogName
+
+    return (
+        <div> 
+            <header className="Underreacted">
+              <h1>Underreacted</h1>
+            </header>
+        </div>
+     );
+
+    //  return (
+    //     <div> 
+    //         <header className={blogName}>
+    //           <h1>{blogName}</h1>
+    //         </header>
+    //     </div>
+    //  );
+}
+
+export default Header;
